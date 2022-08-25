586525065e8f3c4cb1e7bcf17c4c2fe5beba9ac4
kayodegigz <kayogunrinde@gmail.com>
2022-08-25 22:33:28 +0100

    creates a new left node and assigns it to parent

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - inserts a created node as the left child of parent
+ *
+ * @parent: parent of the created node
+ *
+ * @value: int value stored in left node
+ *
+ * Return: returns the pointr to the node created
+ */
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *n_left_node = malloc(sizeof(binary_tree_t));
+
+	if (n_left_node == NULL || parent == NULL)
+	{
+		return (NULL);
+	}
+
+	n_left_node->n = value;
+	n_left_node->parent = parent;
+	if (parent->left == NULL)
+	{
+		parent->left = n_left_node;
+	}
+	else
+	{
+		n_left_node->left = parent->left;
+		parent->left = n_left_node;
+	}
+	return (n_left_node);
+}
