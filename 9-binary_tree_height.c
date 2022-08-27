2810bbea676615008a7f8b3098aafd039b53d2aa
kayodegigz <kayogunrinde@gmail.com>
2022-08-27 22:55:06 +0100

    calculates the hight of the tree

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -17,7 +17,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	right_height = binary_tree_height(tree->right);
 
 	if (left_height >= right_height)
-		return left_height;
+		return (left_height);
 	else
-		return right_height;
+		return (right_height);
 }
