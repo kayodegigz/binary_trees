38211e9dd72c7c6859a66de2538f6abafd902214
kayodegigz <kayogunrinde@gmail.com>
2022-08-27 18:01:10 +0100

    checks if a node is root

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - checks if the node is a root
+ *
+ * @node: node to be checked
+ *
+ * Return: returns 1 or 0
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+
+	if (node->parent == NULL)
+		return (1);
+	return (0);
+}
