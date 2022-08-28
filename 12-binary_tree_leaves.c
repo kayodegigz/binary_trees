4e6345e304febdfff1581028ecc6e58da82bb23a
kayodegigz <kayogunrinde@gmail.com>
2022-08-28 18:35:22 +0100

    fix logical operator error

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -13,7 +13,7 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left == NULL and tree->right == NULL)
+	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
 	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
