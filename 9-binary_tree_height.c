3e91f218c59cee72252f45b3ae423751815c4476
kayodegigz <kayogunrinde@gmail.com>
2022-08-28 11:57:20 +0100

    fixed addition error

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,11 +14,16 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
-
-	if (left_height >= right_height)
-		return (left_height);
-	else
-		return (right_height);
+	/**
+	 * Reason this(below) was done is cos
+	 * alx wants me to do hard things :(.
+	 * If the base case is returning 0 then I cannot just add one for
+	 * every base case cos doing that for an empty tree for example will
+	 * give an incorrect value of 1 instead of 0. So we skip adding 1 to
+	 * the base case(ie where the node being checked is a leaf node and
+	 * add 1 for the rest
+	 */
+	left_height = (tree->left) ? binary_tree_height(tree->left) + 1 : 0;
+	right_height = (tree->right) ? binary_tree_height(tree->right) + 1 : 0;
+	return ((left_height > right_height) ? left_height : right_height);
 }
